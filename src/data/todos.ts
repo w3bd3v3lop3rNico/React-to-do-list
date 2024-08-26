1a519d62ae42948cb8842ce54318c4f22ef7038b
Nicola <nicola.tabai98@gmail.com>
2024-08-26 15:18:00 +0200

    Add fake data & types files

---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+import { Todo } from "../types/todo";
+
+export const missioni: Todo[] = [
+    {
+      id: 1,
+      titolo: "Scopri il villaggio",
+      completata: true
+    },
+    {
+      id: 2,
+      titolo: "Trova la chiave perduta",
+      completata: false
+    },
+    {
+      id: 3,
+      titolo: "Neutralizza il virus",
+      completata: false
+    }
+  ];
+  
---
+++ src/types/todo.ts
@@ -0,0 +1,5 @@
+export interface Todo {
+    id: number;
+    titolo: string;
+    completata: boolean;
+}
