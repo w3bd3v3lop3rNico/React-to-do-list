89cf175fa3884cb4945ba42b8206ceab10c678c2
Nicola <nicola.tabai98@gmail.com>
2024-08-27 16:34:29 +0200

    Add Form component

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+interface AddTodoFormProps {
+  onSubmit: (titolo: string) => void;
+}
+
+export default function AddTodoForm({ onSubmit }: AddTodoFormProps) {
+  const [input, setInput] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!input.trim()) return;
+
+    onSubmit(input);
+    setInput("");
+  }
+
+  return (
+    <form className="d-flex mb-3" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="What's next task to be done?"
+        className="rounded-start p-2 flex-grow-1 text-center"
+      />
+      <label htmlFor="button" className="text-bg-secondary rounded-end">
+        <button
+          id="button"
+          type="submit"
+          className="d-block h-100 text-bg-secondary rounded-end"
+        >
+          Add Task
+        </button>
+      </label>
+    </form>
+  );
+}
